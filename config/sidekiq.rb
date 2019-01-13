8dc04204d92abb4ed288a0f6158e7ab9612db761
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-01-13 19:58:37 +0100

    Configure sidekiq and connect to redis

---
+++ config/sidekiq.rb
@@ -0,0 +1,9 @@
+if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 2 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 10 }
+  end
+end
