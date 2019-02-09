ea2a68736e18ba554fd9520a287212f0c3ae9b72
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-02-09 20:31:33 +0100

    feat: don't show links for beta v2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def beta_v2_enabled?
+    @beta_v2_enabled ||= Content.flipper[:beta_v2].enabled?
+  end
 end
--- engines/content/app/models/content/user.rb
+++ engines/content/app/models/content/user.rb
@@ -1,10 +1,6 @@
 module Content
   class User < ApplicationRecord
-    # Include default devise modules. Others available are:
-    # :lockable, :timeoutable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable,
-           :trackable, :confirmable
+    devise :database_authenticatable, :recoverable, :rememberable, :validatable, :trackable, :confirmable
 
     ROLE_NAMES = %w[super_admin admin user]
 
