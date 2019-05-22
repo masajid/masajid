a8cbcdce22534d2fbbaab36e827833928dc970b6
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-05-22 20:45:35 +0200

    refa: edit configuraion form

--- engines/admin/app/helpers/admin/application_helper.rb
+++ engines/admin/app/helpers/admin/application_helper.rb
@@ -3,5 +3,9 @@ module Admin
     def plural_resource_name(resource_class)
       resource_class.model_name.human(count: 2)
     end
+
+    def options_for_locale(locales: I18n.available_locales)
+      locales.reject(&:blank?).map { |locale| [t("shared.locales.#{locale}"), locale] }
+    end
   end
 end
