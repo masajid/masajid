c22ee845767349cd1aa9fcc245385cdc8ac14bb9
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-07-21 17:33:21 +0200

    refa: active class in admin navigation

--- engines/admin/app/helpers/admin/application_helper.rb
+++ engines/admin/app/helpers/admin/application_helper.rb
@@ -7,5 +7,11 @@ module Admin
     def options_for_locale(locales: I18n.available_locales)
       locales.reject(&:blank?).map { |locale| [t("shared.locales.#{locale}"), locale.to_s] }
     end
+
+    def nav_item(text:, path:, active:)
+      content_tag(:li, class: 'nav-item') do
+        link_to(text, path, class: ['nav-link', active && 'active'])
+      end
+    end
   end
 end
