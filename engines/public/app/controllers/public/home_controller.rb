5d7c477ec49ae321b1431e1ee25f3ba8245734f6
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-07-21 16:01:39 +0200

    feat: add dynamic articles to home page

--- engines/public/app/controllers/public/home_controller.rb
+++ engines/public/app/controllers/public/home_controller.rb
@@ -4,6 +4,7 @@ module Public
   class HomeController < ApplicationController
     def index
       @slider = current_account.slider
+      @articles = Content::Article.scoped_to(current_account)
     end
   end
 end
