cc2c9ead108df8ffb7a10c2a6da3bbb5c2c23f13
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-08-03 10:55:32 +0200

    feat: add differnet menu for small devices

--- engines/public/app/assets/javascripts/public/application.js
+++ engines/public/app/assets/javascripts/public/application.js
@@ -18,6 +18,12 @@
 //= require_tree .
 
 $(function() {
+  'use strict'
+
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.offcanvas-collapse').toggleClass('open')
+  })
+
   $('.locale_selector select').change(function() {
     this.form.submit();
   });
