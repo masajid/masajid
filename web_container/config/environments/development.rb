dc85b746a54afe2daedf60e02d94073a5c79510c
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-09-15 21:28:13 +0200

    fix: hosts

--- web_container/config/environments/development.rb
+++ web_container/config/environments/development.rb
@@ -64,5 +64,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts += %w[masajid.local al-nour.local ccicbcn.masajid.local]
+  config.hosts += %w[masajid.local al-nour.masajid.local ccicbcn.masajid.local]
 end
