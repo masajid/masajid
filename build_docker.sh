763438f5dce1b7eb43c15c5c26ee1d3f85010bc3
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-10-26 21:27:14 +0200

    added dockerignore file

--- build_docker.sh
+++ build_docker.sh
@@ -8,6 +8,5 @@ echo $RAILS_MASTER_KEY
 
 docker build \
   -f Dockerfile.prod \
-  --build-arg="RAILS_ENV=production" \
   --build-arg="RAILS_MASTER_KEY=${RAILS_MASTER_KEY}" \
   -t ${APP_NAME}:${VERSION} .
