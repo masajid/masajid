e377d6158356f15485754448a15dfd94893be0f5
Moustafa Abdelhamid <mou.abdelhamid@gmail.com>
2019-10-26 23:51:55 +0200

    Remove unused echo

--- build_docker.sh
+++ build_docker.sh
@@ -4,8 +4,6 @@ APP_NAME=$(cat name)
 VERSION=$(cat version)
 RAILS_MASTER_KEY=$(cat web_container/config/master.key)
 
-echo $RAILS_MASTER_KEY
-
 docker build \
   -f Dockerfile.prod \
   --build-arg="RAILS_MASTER_KEY=${RAILS_MASTER_KEY}" \
