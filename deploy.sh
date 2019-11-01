3d0302b6d8e1083c18073ac6647f7da06d0e2929
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-11-02 00:15:12 +0100

    fix: cd to travis working directoy before deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 set -ex
 
+cd $TRAVIS_BUILD_DIR
 openssl aes-256-cbc -K $encrypted_01c4308a40f4_key -iv $encrypted_01c4308a40f4_iv -in service-account.json.enc -out service-account.json -d
 curl https://sdk.cloud.google.com | bash > /dev/null;
 source $HOME/google-cloud-sdk/path.bash.inc
