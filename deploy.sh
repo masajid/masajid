8f520706f1234252406b09f6c0fb00b142ac1841
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-11-02 00:19:26 +0100

    chore: travis_ci_test_deploy

--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,4 @@ docker build \
 docker push eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA}
 docker push eu.gcr.io/${GKE_PROJECT_ID}/masajid:latest
 
-kubectl set image deployment/masajid-deployment masajid=eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA}
+kubectl set image deployment/masajid-frontend masajid=eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA}
