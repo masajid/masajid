2cb9792afc85828a3a0adeb9665d38f218d83f50
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-11-10 14:06:57 +0100

    feat: add before deployment block

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,5 @@ docker build \
 gcloud docker -- push eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA}
 gcloud docker -- push eu.gcr.io/${GKE_PROJECT_ID}/masajid:latest
 
-kubectl set image deployment/masajid-frontend masajid=eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA}
+kubectl set image deployment/masajid-frontend masajid-frontend=eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA} --record
+kubectl set image deployment/masajid-sidekiq masajid-sidekiq=eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA} --record
