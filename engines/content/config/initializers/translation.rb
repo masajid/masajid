b83bf298bbb6b2e26e2d8d670548f61a05f47e6e
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-11-14 18:07:47 +0100

    feat: automate translations with TranslationIO

---
+++ engines/content/config/initializers/translation.rb
@@ -0,0 +1,9 @@
+TranslationIO.configure do |config|
+  config.api_key        = 'c81a00b9194a42959d61f5b0c9441781'
+  config.source_locale  = 'en'
+  config.target_locales = %w[fr de es]
+
+  config.disable_gettext = true
+  config.yaml_locales_path = Content::Engine.root.join('config/locales')
+  config.metadata_path     = Content::Engine.root.join('config/locales/.translation_io')
+end
--- engines/content/lib/content.rb
+++ engines/content/lib/content.rb
@@ -15,6 +15,7 @@ require 'bootstrap-email'
 require 'ahoy_matey'
 require 'draper'
 require 'image_processing'
+require 'translation'
 
 module Content
   require 'content/engine'
