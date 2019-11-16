63475235848e97fc499986b15c322f3a941df791
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-11-16 21:45:11 +0100

    feat: add more languanges

--- engines/content/config/initializers/translation.rb
+++ engines/content/config/initializers/translation.rb
@@ -1,7 +1,7 @@
 TranslationIO.configure do |config|
   config.api_key        = 'c81a00b9194a42959d61f5b0c9441781'
   config.source_locale  = 'en'
-  config.target_locales = %w[fr de es]
+  config.target_locales = %w[de fr es ca it tr ar]
 
   config.disable_gettext = true
   config.yaml_locales_path = Content::Engine.root.join('config/locales')
