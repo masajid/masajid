6861a613d3dc67e5a7034af24c1aa01d98653792
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-12-07 20:10:47 +0100

    refa: add space

--- engines/content/config/initializers/translation.rb
+++ engines/content/config/initializers/translation.rb
@@ -3,7 +3,7 @@ TranslationIO.configure do |config|
   config.source_locale  = 'en'
   config.target_locales = %w[de fr es ca it tr ar]
 
-  config.disable_gettext = true
+  config.disable_gettext   = true
   config.yaml_locales_path = Content::Engine.root.join('config/locales')
   config.metadata_path     = Content::Engine.root.join('config/locales/.translation_io')
 
