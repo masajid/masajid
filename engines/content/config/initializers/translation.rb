4a34416ee9b34482b8048a46d5b58c6ebac8972d
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-12-07 20:04:32 +0100

    feat: specify translation keys that should not be touched by translators

--- engines/content/config/initializers/translation.rb
+++ engines/content/config/initializers/translation.rb
@@ -6,4 +6,9 @@ TranslationIO.configure do |config|
   config.disable_gettext = true
   config.yaml_locales_path = Content::Engine.root.join('config/locales')
   config.metadata_path     = Content::Engine.root.join('config/locales/.translation_io')
+
+  # translation keys that should not be touched by translators
+  config.localization_key_prefixes = %w[
+    validates_timeliness.error_value_formats
+  ]
 end
