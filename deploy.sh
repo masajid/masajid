3be16d7c3127c8a75fa8a43ca53241331786244e
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-12-09 23:50:33 +0100

    chore: travis ci test automated deployment #16

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+set -ex
+
+docker build \
+  -f Dockerfile.prod \
+  --build-arg="RAILS_MASTER_KEY=${RAILS_MASTER_KEY}" \
+  -t eu.gcr.io/${GKE_PROJECT_ID}/masajid:latest \
+  -t eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA} .
+
+docker push -- eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA}
+docker push -- eu.gcr.io/${GKE_PROJECT_ID}/masajid:latest
+
+kubectl set image deployment/masajid-frontend masajid-frontend=eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA} --record
+kubectl set image deployment/masajid-sidekiq masajid-frontend=eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA} --record
