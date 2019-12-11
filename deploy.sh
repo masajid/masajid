cd7f45c1f4cc2ff64a25752a788fe473260a6b39
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-12-11 01:18:05 +0100

    fix: docker images names

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@ set -ex
 docker build \
   -f Dockerfile.prod \
   --build-arg="RAILS_MASTER_KEY=${RAILS_MASTER_KEY}" \
-  -t eu.gcr.io/${GKE_PROJECT_ID}/masajid:latest \
-  -t eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA} .
+  -t eu.gcr.io/${GKE_PROJECT_ID}/masajid-frontend:latest \
+  -t eu.gcr.io/${GKE_PROJECT_ID}/masajid-frontend:${SHA} .
 
-docker push -- eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA}
-docker push -- eu.gcr.io/${GKE_PROJECT_ID}/masajid:latest
+docker push -- eu.gcr.io/${GKE_PROJECT_ID}/masajid-frontend:${SHA}
+docker push -- eu.gcr.io/${GKE_PROJECT_ID}/masajid-frontend:latest
 
-kubectl set image deployment/masajid-frontend masajid-frontend=eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA} --record
-kubectl set image deployment/masajid-sidekiq masajid-frontend=eu.gcr.io/${GKE_PROJECT_ID}/masajid:${SHA} --record
+kubectl set image deployment/masajid-frontend masajid-frontend=eu.gcr.io/${GKE_PROJECT_ID}/masajid-frontend:${SHA} --record
+kubectl set image deployment/masajid-sidekiq masajid-frontend=eu.gcr.io/${GKE_PROJECT_ID}/masajid-frontend:${SHA} --record
