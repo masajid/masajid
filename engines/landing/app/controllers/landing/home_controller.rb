46e76c51eab01778cdfc3c81a7a44c75f07ffda7
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2019-12-22 19:34:25 +0100

    feat: add mosque link to map marker

--- engines/landing/app/controllers/landing/home_controller.rb
+++ engines/landing/app/controllers/landing/home_controller.rb
@@ -2,6 +2,8 @@ require_dependency 'landing/application_controller'
 
 module Landing
   class HomeController < ApplicationController
+    include Content::AccountsHelper
+
     layout 'landing/landing'
 
     def index
