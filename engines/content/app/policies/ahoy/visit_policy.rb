a842d263fbe62491b5948861cfb252ff2561be09
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2020-02-02 19:32:35 +0100

    feat: adjust number of visits

--- engines/content/app/policies/ahoy/visit_policy.rb
+++ engines/content/app/policies/ahoy/visit_policy.rb
@@ -3,11 +3,11 @@ module Ahoy
     class Scope < Scope
       def resolve
         if user.super_admin?
-          # TODO: only visit from landing page
-          scope.all
+          scope.where('landing_page ~* ?', "http(s?)://#{ENV['PROJECT_HOSTNAME']}\s*")
         elsif user.admin?
-          # TODO: use ahoy referrer and match account domain as well
-          scope.where('landing_page LIKE ?', "%#{user.account.subdomain}%")
+          account = user.account
+
+          scope.where('landing_page ~* ?', "\s*(#{account.subdomain}.#{ENV['PROJECT_HOSTNAME']}|#{account.domain})\s*")
         else
           scope.none
         end
