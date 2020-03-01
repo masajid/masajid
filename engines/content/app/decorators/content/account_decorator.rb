5be6dcb51dea4657a6ba402b394ed5eb336eea84
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2020-03-02 00:10:29 +0100

    fix: address in map

--- engines/content/app/decorators/content/account_decorator.rb
+++ engines/content/app/decorators/content/account_decorator.rb
@@ -2,6 +2,15 @@ module Content
   class AccountDecorator < ApplicationDecorator
     delegate_all
 
+    def display_address_short
+      [
+        object.address1,
+        object.zip_code,
+        object.city_name,
+        object.country.name
+      ].compact.join(' ')
+    end
+
     def display_address
       [
         object.address1,
