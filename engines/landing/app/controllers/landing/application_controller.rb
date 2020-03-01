d07923562b6b9033fbefa7e233b8bcdc905b60cf
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2020-03-01 09:44:21 +0100

    feat: remove unused helper methods

--- engines/landing/app/controllers/landing/application_controller.rb
+++ engines/landing/app/controllers/landing/application_controller.rb
@@ -13,15 +13,5 @@ module Landing
     def after_sign_in_path_for(resource)
       stored_location_for(resource) || '/admin'
     end
-
-    def google_analytics_tracking_id
-      @google_analytics_tracking_id ||= ENV['GOOGLE_ANALYTICS_TRACKING_ID']
-    end
-    helper_method :google_analytics_tracking_id
-
-    def render_google_analytics?
-      Rails.env.production? && google_analytics_tracking_id.present?
-    end
-    helper_method :render_google_analytics?
   end
 end
