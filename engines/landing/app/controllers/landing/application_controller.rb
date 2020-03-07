e7488ca22f62aa936dc91f722479c7d2bf8c857c
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2020-03-07 17:04:37 +0100

    feat: add rtl to landing engine

--- engines/landing/app/controllers/landing/application_controller.rb
+++ engines/landing/app/controllers/landing/application_controller.rb
@@ -7,10 +7,16 @@ module Landing
     include Landing::ControllerHelpers::Locale
     include Landing::ControllerHelpers::SeoContent
 
+    layout :set_layout
+
     private
 
     def after_sign_in_path_for(resource)
       stored_location_for(resource) || '/admin'
+    end
+
+    def set_layout
+      I18n.locale == :ar ? 'landing/rtl/application' : 'landing/application'
     end
   end
 end
