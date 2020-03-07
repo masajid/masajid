aef339eca88863b75de3bf6ee5021859025bfb13
Gharbi Mohammed <mohammed.gharbi@eventinc.de>
2020-03-07 12:55:42 +0100

    feat: adjust with previous features

--- engines/landing/lib/landing/engine.rb
+++ engines/landing/lib/landing/engine.rb
@@ -1,9 +1,5 @@
 module Landing
   class Engine < ::Rails::Engine
     isolate_namespace Landing
-
-    initializer :precompile_landing_assets do |app|
-      app.config.assets.precompile += %w[landing/landing.js landing/landing.css landing/*]
-    end
   end
 end
