452a016bf8ffc9aa39c7a5d830fb447d29dec06c
Gharbi Mohammed <mgharbik@gmail.com>
2020-04-11 02:04:40 +0200

    fix: geocoding

--- engines/content/app/models/content/address.rb
+++ engines/content/app/models/content/address.rb
@@ -17,12 +17,16 @@ module Content
     validates :region_name, presence: true
     validates :country_id, presence: true
 
-    before_validation :geolocate
+    before_validation :geolocate, if: -> { changed? && to_s.present? }
+
+    def to_s
+      [ address1, zip_code, city_name, country&.name].compact.join(' ')
+    end
 
     private
 
     def geolocate
-      result = addressable && ::Geocoder.coordinates(addressable.decorate.display_address_short)
+      result = ::Geocoder.coordinates(to_s)
 
       if result.present?
         self.latitude = result.first
