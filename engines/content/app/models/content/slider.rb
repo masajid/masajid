4a6942f0e070bdacfc9e99ddd2e02f0f4d0fc3e6
Gharbi Mohammed <mgharbik@gmail.com>
2020-05-02 14:10:43 +0200

    feat: slider photo

--- engines/content/app/models/content/slider.rb
+++ engines/content/app/models/content/slider.rb
@@ -1,7 +1,13 @@
 module Content
   class Slider < ApplicationRecord
-    has_one_attached :photo
+    attr_accessor :remove_photo
 
+    after_save :purge_photo, if: :remove_photo
+    private def purge_photo
+      photo.purge_later
+    end
+
+    has_one_attached :photo
     belongs_to :account
   end
 end
