9682fb26304cd25b13cc33084e01f9536cd20e63
Gharbi Mohammed <mgharbik@gmail.com>
2020-05-09 18:33:35 +0200

    feat: add fullbacks

--- web_container/config/application.rb
+++ web_container/config/application.rb
@@ -16,6 +16,7 @@ module Masajid
 
     config.i18n.default_locale = :en
     config.i18n.available_locales = %i[en de nl fr es ca it tr ar]
+    config.i18n.fallbacks = config.i18n.available_locales
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
