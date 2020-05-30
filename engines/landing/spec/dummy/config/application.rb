5ccf354f4e7add9a676d9f7cd1d29ec48e8a6893
Gharbi Mohammed <mgharbik@gmail.com>
2020-05-30 08:35:34 +0200

    fix: use db schema from the web_container

--- engines/landing/spec/dummy/config/application.rb
+++ engines/landing/spec/dummy/config/application.rb
@@ -17,5 +17,8 @@ module Dummy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Use database schema from the web_container
+    config.paths['db'] = ['../../../../web_container/db']
   end
 end
