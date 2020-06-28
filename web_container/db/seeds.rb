ad830476ab059a9bfaef4dd4ad74c190f5b69125
Gharbi Mohammed <mgharbik@gmail.com>
2020-06-28 23:25:34 +0200

    feat: use credentials for multi environment

--- web_container/db/seeds.rb
+++ web_container/db/seeds.rb
@@ -1,7 +1,7 @@
 # Create super admin user
 Content::User.create(
-  email: Rails.application.credentials[Rails.env.to_sym][:super_admin_email],
-  password: Rails.application.credentials[Rails.env.to_sym][:super_admin_password],
+  email: Rails.application.credentials.super_admin_email,
+  password: Rails.application.credentials.super_admin_password,
   role: 'super_admin',
   confirmed_at: Time.current
 )
