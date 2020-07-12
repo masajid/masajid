274d0a933d4e91f8d9146866d0c4af328740ea92
Gharbi Mohammed <mgharbik@gmail.com>
2020-07-12 13:52:41 +0200

    test: fix specs

--- engines/content/lib/content/dependencies/test.rb
+++ engines/content/lib/content/dependencies/test.rb
@@ -1,4 +1,5 @@
 group :test do
+  gem 'capybara', '~> 3.32.2'
   gem 'factory_bot_rails', '~> 5.2.0'
   gem 'faker', git: 'https://github.com/faker-ruby/faker.git', branch: 'master'
   gem 'rspec-rails', '~> 4.0.1'
